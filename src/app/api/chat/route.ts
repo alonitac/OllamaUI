2587415b7a6863818401b5afd422c74501460107
tamer <tmeraslan1@gmail.com>
2025-09-07 23:56:59 +0300

    route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
   if (data?.images?.length) {
     try {
       const bucket = process.env.AWS_S3_BUCKET!;
-      const yoloService = "yolo:8081";
+      const yoloService = "http://yolo:8081";
       // const yoloService = process.env.YOLO_SERVICE ||  "http://localhost:8081";
 
       if (!bucket || !yoloService) {
@@ -78,14 +78,17 @@ export async function POST(req: Request) {
        );
    
       const predictionResponse = await fetch(
-        `http://yolo:8081/predict?img=${encodeURIComponent(key)}`,
+        `http://yolo:8081/predict?img_url=${encodeURIComponent(key)}`,
         { method: 'POST' },
       );
+
+
 
       //  const predictionResponse = await fetch( 
       //  `${yoloService.replace(/\/+$/, '')}/predict?img=${encodeURIComponent(key)}`, 
       //  { method: 'POST' }, 
       //  );
+   
 
       if (!predictionResponse.ok) {
         const text = await predictionResponse.text().catch(() => '');
