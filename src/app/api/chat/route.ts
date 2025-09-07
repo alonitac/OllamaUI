87834bc455c47457b8f342e2f911783b000bf7be
tamer <tmeraslan1@gmail.com>
2025-09-08 01:28:04 +0300

    route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,7 +2,7 @@
 
 // route.ts
 
-import { randomUUID } from 'crypto';
+// import { randomUUID } from 'crypto';
 import { routeModule } from 'next/dist/build/templates/app-page';
 import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
